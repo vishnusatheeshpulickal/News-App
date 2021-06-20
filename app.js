2a322ce8ecb941cd1f390127a493ed79a1a6f430
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-06-20 16:40:07 +0530

    Footer is added

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ const newsRouter = require('./src/routes/news')
 app.use('/',newsRouter)
 app.use('/article',newsRouter)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 app.listen(port,()=>console.log(`Listening port ${port}...`))
